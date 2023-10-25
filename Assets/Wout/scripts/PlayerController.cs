f56e290660069da086753206efa5aa7597632401
WoutVanBoxem <woutvanboxem02@gmail.com>
2023-10-25 11:29:54 +0200

    wandelanimatie afgemaakt

--- Assets/Wout/scripts/PlayerController.cs
+++ Assets/Wout/scripts/PlayerController.cs
@@ -1,35 +1,45 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 2.0f; // Snelheid van de speler
-    private Animator animator; // Animator component
-    public bool isWalking = false; // Nieuwe isWalking eigenschap
+    public float speed = 2.0f;
+    private Animator animator;
+    public bool isWalking = false;
+    private Rigidbody rb;
 
     void Start()
     {
-        // Verkrijg de Animator component
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // Verkrijg input van de speler
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
-        // Bepaal de bewegingsrichting
         Vector3 movement = new Vector3(horizontal, 0, vertical);
         movement.Normalize();
 
-        // Verplaats de speler
-        transform.Translate(movement * Speed * Time.deltaTime, Space.World);
-
-        // Update de Speed parameter in de Animator
         animator.SetFloat("Speed", movement.magnitude);
 
         // Update de isWalking eigenschap
         isWalking = movement.magnitude > 0;
         animator.SetBool("isWalking", isWalking);  // Zet de isWalking parameter in de Animator
+
+        // Log de isWalking en Speed eigenschap in de console
+        Debug.Log("isWalking: " + isWalking);
+        Debug.Log("Speed: " + animator.GetFloat("Speed"));
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        movement.Normalize();
+
+        Vector3 moveDestination = transform.position + movement * speed * Time.fixedDeltaTime;
+        rb.MovePosition(moveDestination);
     }
 }
